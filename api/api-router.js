ae4d5f613ba06de1f94ba22a667f79257a60a679
Gatrrr <davidgatrmacaulay@gmail.com>
2019-11-11 09:37:55 -0800

    idk

--- api/api-router.js
+++ api/api-router.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcryptjs');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+
+router.post('/hash', (req, res) => {
+  const  pw = req.body.password;
+
+  //hash pw
+
+  req.body.password = bcrypt.hashSync(pw, 12);
+
+  //return to user in an object
+
+  res.status(200).json({password: `${pw}`, hashed: `${req.body.password}`});
+})
 
 router.use('/auth', authRouter);
 router.use('/users', usersRouter);
